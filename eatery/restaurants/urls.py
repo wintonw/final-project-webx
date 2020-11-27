d8bda696d6b0843ab1f167e51c2adbc781b849bc
WintonMac <winton@wintonw.com>
2020-11-26 23:27:15 -0800

    logout

--- eatery/restaurants/urls.py
+++ eatery/restaurants/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path, reverse
 from . import views
 
 # app name
-app_name = 'main'
+app_name = 'restaurants'
 urlpatterns = [
     path('', views.index, name='index'),
-    path('login/', views.login, name='login'),
+    path('login/', views.loginPage, name='login'),
     path('signUp/', views.signUp, name='signUp'),
+    path('logout/', views.logoutUser, name='logout'),
     # path('weight/', , name='contactMe'),
 ]
--- eatery/restaurants/views.py
+++ eatery/restaurants/views.py
@@ -22,16 +22,31 @@ def signUp(request):
             messages.success(request, "Account was created for " + user)
 
             form.save()
-            return redirect('/login/')
+            return redirect('restaurants:login')
 
     context = {'form': form}
     return render(request, 'signUp.html', context)
 
 
-def login(request):
+def loginPage(request):
 
     if request.method == 'POST':
-        request.POST.get('username')
-        request.POST.get('password')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            # good exist
+            login(request, user)
+            return redirect('restaurants:index')
+        else:
+            messages.info(request, 'Username or Password is INCORRECT')
+
     context = {}
     return render(request, 'login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('restaurants:login')
