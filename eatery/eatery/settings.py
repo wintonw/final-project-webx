e5d7cfdcad2ca62b02ee47776570c54ee177fd09
WintonMac <winton@wintonw.com>
2020-11-29 15:00:53 -0800

    static route

--- eatery/eatery/settings.py
+++ eatery/eatery/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'eatery/static'),
 )
 
 PHONENUMBER_DB_FORMAT = 'NATIONAL'
